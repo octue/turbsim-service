3b52e7375b9ffda953616a21cedfe61f4898191f
cortadocodes <cortado.codes@protonmail.com>
2021-09-14 20:04:48 +0100

    REF: Pull subprocess and cloud save into functions

--- app.py
+++ app.py
@@ -11,21 +11,28 @@ def run(analysis):
     start_datetime = datetime.datetime.now()
     input_file = list(analysis.input_manifest.get_dataset("turbsim_input").files)[0]
 
-    analysis.logger.info("Starting turbulence simulation.")
+    output_file = _run_and_log_turbsim(analysis, input_file)
+    analysis.output_manifest.get_dataset("turbsim_output").add(output_file)
 
+    _save_output_to_cloud(analysis, output_file, start_datetime)
+    analysis.finalise()
+
+
+def _run_and_log_turbsim(analysis, input_file):
+    analysis.logger.info("Starting turbulence simulation.")
     command = ["turbsim", input_file.get_local_path()]
     process = Popen(command, stdout=PIPE, stderr=STDOUT)
-    Thread(target=log_lines_from_stream, args=[process.stdout, analysis.logger]).start()
+    Thread(target=_log_lines_from_stream, args=[process.stdout, analysis.logger]).start()
     process.wait()
 
     if process.returncode != 0:
         raise CalledProcessError(returncode=process.returncode, cmd="".join(command))
 
     analysis.logger.info("Finished turbulence simulation.")
+    return Datafile(path=input_file.get_local_path() + ".bts", labels=["turbsim"])
 
-    output_file = Datafile(path=input_file.get_local_path() + ".bts", labels=["turbsim"])
-    analysis.output_manifest.get_dataset("turbsim_output").add(output_file)
 
+def _save_output_to_cloud(analysis, output_file, start_datetime):
     output_cloud_path = storage.path.generate_gs_path(
         os.environ["BUCKET_NAME"],
         "turbsim",
@@ -39,10 +46,8 @@ def run(analysis):
     output_file.path = output_cloud_path
     analysis.logger.info("Output saved.")
 
-    analysis.finalise()
 
-
-def log_lines_from_stream(stream, logger):
+def _log_lines_from_stream(stream, logger):
     with stream:
         for line in iter(stream.readline, b''):
             logger.info(line.decode().strip())
