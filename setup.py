8c8a9824a77e0c51f27caec24315dc75c678519b
cortadocodes <cortado.codes@protonmail.com>
2022-04-21 17:45:46 +0100

    DOC: Add cortadocodes as author

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name="turbsim-service",
     version="0.1.0",
+    author="cortadocodes <cortado.codes@protonmail.com>",
     py_modules=["app"],
     install_requires=[
         "coolname>=1.1,<2",
