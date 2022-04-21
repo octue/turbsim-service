26f854a57412bfd5bab95ef518b9332d658045e1
cortadocodes <cortado.codes@protonmail.com>
2022-04-21 12:11:35 +0100

    TST: Update deployment test according to new octue version

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,37 +1,37 @@
 import os
 import unittest
-from octue.cloud.pub_sub.service import Service
-from octue.resources.service_backends import GCPPubSubBackend
-from octue.resources import Datafile, Manifest, Dataset
+
+from octue.resources import Datafile, Manifest, Dataset, Child
 from octue.cloud import storage
+
+
+REPOSITORY_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 
 class TestDeployment(unittest.TestCase):
 
     @unittest.skipUnless(
-        condition=os.getenv("RUN_DEPLOYMENT_TESTS", "").lower() == "true",
-        reason="'RUN_DEPLOYMENT_TESTS' environment variable is False or not present.",
+        condition=os.getenv("RUN_DEPLOYMENT_TESTS", "0").lower() == "1",
+        reason="'RUN_DEPLOYMENT_TESTS' environment variable is 0 or not present.",
     )
     def test_cloud_run_integration(self):
         """Test that the Google Cloud Run integration works, providing a service that can be asked questions and send
         responses. An input dataset from Google Cloud Storage is used for this test.
         """
-        PROJECT_NAME = os.environ["TEST_PROJECT_NAME"]
-        SERVICE_ID = "octue.services.c3b47b47-cdfa-433d-b5a8-47a58f3bf7cb"
-
-        input_file = Datafile(
-            path=storage.path.generate_gs_path(os.environ["TEST_BUCKET_NAME"], "testing", "TurbSim.inp"),
-            project_name=PROJECT_NAME,
-        )
+        project_name = os.environ["TEST_PROJECT_NAME"]
+        service_id = "octue.services.c3b47b47-cdfa-433d-b5a8-47a58f3bf7cb"
 
         input_manifest = Manifest(
-            datasets=[Dataset(name="turbsim", files=[input_file])],
-            keys={"turbsim": 0},
+            datasets={"turbsim": Dataset.from_local_directory(os.path.join(REPOSITORY_ROOT, "data", "turbsim"))}
         )
 
-        asker = Service(backend=GCPPubSubBackend(project_name=PROJECT_NAME))
-        subscription, _ = asker.ask(service_id=SERVICE_ID, input_manifest=input_manifest)
-        answer = asker.wait_for_answer(subscription, timeout=100000)
+        child = Child(
+            name="turbsim-service",
+            id=service_id,
+            backend={"name": "GCPPubSubBackend", "project_name": project_name},
+        )
+
+        answer = child.ask(input_manifest=input_manifest, timeout=360)
 
         self.assertIsNone(answer["output_values"])
         self.assertIsNotNone(answer["output_manifest"])
@@ -39,4 +39,4 @@ class TestDeployment(unittest.TestCase):
         output_file = list(answer['output_manifest'].datasets[0].files)[0]
         self.assertIn("turbsim", output_file.labels)
 
-        storage.client.GoogleCloudStorageClient(PROJECT_NAME).delete(cloud_path=output_file.absolute_path)
+        storage.client.GoogleCloudStorageClient(project_name).delete(cloud_path=output_file.absolute_path)
