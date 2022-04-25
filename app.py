b46f76f2d23e087035889d837e978677c4d2214d
cortadocodes <cortado.codes@protonmail.com>
2022-04-25 17:53:25 +0100

    ENH: Always use "TurbSim.bts" as output file name

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import os
+import shutil
 
 import coolname
 from octue.cloud import storage
@@ -9,6 +10,9 @@ from octue.utils.processes import run_subprocess_and_log_stdout_and_stderr
 
 
 logger = logging.getLogger(__name__)
+
+
+OUTPUT_FILENAME = "TurbSim.bts"
 
 
 def run(analysis):
@@ -25,6 +29,12 @@ def run(analysis):
 
     logger.info("Starting turbsim analysis.")
     run_subprocess_and_log_stdout_and_stderr(command=["turbsim", input_file.local_path], logger=logger)
+
+    original_output_file_path = os.path.splitext(input_file.local_path)[0] + ".bts"
+    new_output_file_path = os.path.join(os.path.split(input_file.local_path)[0], OUTPUT_FILENAME)
+
+    if original_output_file_path != new_output_file_path:
+        shutil.copy(original_output_file_path, new_output_file_path)
 
     # Instantiate a datafile for the output file (it has the same name as the input file but with a ".bts" extension).
     output_file = Datafile(
