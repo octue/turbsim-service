d5472d19b35137a592d1ebb9f12883abd3ad3643
cortadocodes <cortado.codes@protonmail.com>
2022-05-02 17:33:50 +0100

    DEP: Use octue==0.23.0

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     py_modules=["app"],
     install_requires=[
         "coolname>=1.1,<2",
-        "octue @ https://github.com/octue/octue-sdk-python/archive/fix/miscellaneous-fixes.zip",
+        "octue==0.23.0",
     ],
 )
