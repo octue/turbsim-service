9a526a44ec642b34b21fad481408f5bbe1ce2893
cortadocodes <cortado.codes@protonmail.com>
2022-05-02 18:23:56 +0100

    FIX: Download to temporary directory; move instead of copy output file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 import logging
 import os
-import shutil
+import tempfile
 
 import coolname
 from octue.cloud import storage
@@ -12,7 +12,8 @@ from octue.utils.processes import run_subprocess_and_log_stdout_and_stderr
 logger = logging.getLogger(__name__)
 
 
-OUTPUT_FILENAME = "TurbSim.bts"
+OUTPUT_EXTENSION = ".bts"
+OUTPUT_FILENAME = "TurbSim" + OUTPUT_EXTENSION
 
 
 def run(analysis):
@@ -23,28 +24,30 @@ def run(analysis):
     """
     start_datetime = datetime.datetime.now()
 
-    input_dataset = analysis.input_manifest.datasets["turbsim"]
-    input_dataset.download_all_files()
-    input_file = input_dataset.files.one()
+    with tempfile.TemporaryDirectory() as temporary_directory:
+        input_dataset = analysis.input_manifest.datasets["turbsim"]
+        input_dataset.download_all_files(temporary_directory)
+        input_file = input_dataset.files.one()
 
-    logger.info("Starting turbsim analysis.")
-    run_subprocess_and_log_stdout_and_stderr(command=["turbsim", input_file.local_path], logger=logger)
+        logger.info("Starting turbsim analysis.")
+        run_subprocess_and_log_stdout_and_stderr(command=["turbsim", input_file.local_path], logger=logger)
 
-    original_output_file_path = os.path.splitext(input_file.local_path)[0] + ".bts"
-    new_output_file_path = os.path.join(os.path.split(input_file.local_path)[0], OUTPUT_FILENAME)
+        old_output_filename = os.path.splitext(input_file.local_path)[0] + OUTPUT_EXTENSION
+        new_output_filename = os.path.join(temporary_directory, OUTPUT_FILENAME)
+        os.rename(old_output_filename, new_output_filename)
 
-    if original_output_file_path != new_output_file_path:
-        shutil.copy(original_output_file_path, new_output_file_path)
+        # Instantiate a datafile for the output file.
+        output_file = Datafile(
+            path=new_output_filename,
+            timestamp=start_datetime,
+            labels=["turbsim", "output"],
+        )
 
-    # Instantiate a datafile for the output file (it has the same name as the input file but with a ".bts" extension).
-    output_file = Datafile(
-        path=os.path.splitext(input_file.local_path)[0] + ".bts",
-        timestamp=start_datetime,
-        labels=["turbsim", "output"],
-    )
+        analysis.output_manifest.datasets["turbsim"] = Dataset(name="turbsim", path=temporary_directory)
+        analysis.output_manifest.datasets["turbsim"].add(output_file)
 
-    analysis.output_manifest.datasets["turbsim"] = Dataset(path=os.path.dirname(input_file.local_path))
-    analysis.output_manifest.datasets["turbsim"].add(output_file)
+        analysis.finalise(
+            upload_output_datasets_to=storage.path.join(analysis.output_location, coolname.generate_slug())
+        )
 
-    analysis.finalise(upload_output_datasets_to=storage.path.join(analysis.output_location, coolname.generate_slug()))
     logger.info("Finished turbsim analysis.")
