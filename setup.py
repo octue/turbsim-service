07a2cfc34c29a287e9e1d7cc861df8605259e3d6
cortadocodes <cortado.codes@protonmail.com>
2022-05-03 16:03:52 +0100

    DEP: Use octue branch

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     py_modules=["app"],
     install_requires=[
         "coolname>=1.1,<2",
-        "octue @ https://github.com/octue/octue-sdk-python/archive/fix/validate-output-location-outside-of-twine.zip",
+        "octue @ https://github.com/octue/octue-sdk-python/archive/fix/ensure-signed-urls-can-be-accessed-without-permissions.zip",
     ],
 )
