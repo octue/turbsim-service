e90834a8d6dd0608fb819801161729713fc7c95c
cortadocodes <cortado.codes@protonmail.com>
2022-09-15 11:20:43 +0100

    ENH: Simplify analysis finalisation

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name="turbsim-service",
-    version="0.1.0",
+    version="0.1.1",
     author="cortadocodes <cortado.codes@protonmail.com>",
     py_modules=["app"],
     install_requires=[
         "coolname>=1.1,<2",
-        "octue==0.26.0",
+        "octue @ https://github.com/octue/octue-sdk-python/archive/enhancement/automatically-upload-output-datasets-to-output-location.zip",
     ],
 )
