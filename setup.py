846d2a072254869a31944ff0e7fe0a4c698c9552
cortadocodes <cortado.codes@protonmail.com>
2023-12-04 10:41:03 +0000

    DEP: Update `octue` version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name="turbsim-service",
-    version="0.1.1",
-    author="cortadocodes <cortado.codes@protonmail.com>",
+    version="0.2.0",
+    author="Marcus Lugg <marcus@octue.com>",
     py_modules=["app"],
     install_requires=[
         "coolname>=1.1,<2",
-        "octue==0.35.1",
+        "octue==0.50.0",
     ],
 )
